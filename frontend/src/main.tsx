681b6271fec21ea0f7f8bcacdefc7f8ad9590a84
Mak241288 <khateeb.moh@gmail.com>
2026-08-27 01:27:50 +0300

    feat(monitoring): integrate @sentry/react in frontend and sentry-sdk in backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,6 +7,17 @@ import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 
 import { WorkoutSessionProvider } from './context/WorkoutSessionContext.tsx'
 import { registerAutoUpdateServiceWorker } from './utils/pwaUpdate.ts'
+
+Sentry.init({
+  dsn: "https://d86e72394ab3886fccdeb4ec8631e6c7@o4511979386044416.ingest.us.sentry.io/4511979386306560",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
