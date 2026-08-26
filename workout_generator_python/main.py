62fcb43b484941c8434f20f3eb2af78baf863560
Mak241288 <khateeb.moh@gmail.com>
2026-08-27 01:41:01 +0300

    perf(monitoring): update Sentry tracesSampleRate and traces_sample_rate to 0.1 across frontend and backend

--- workout_generator_python/main.py
+++ workout_generator_python/main.py
@@ -2,7 +2,7 @@ import sentry_sdk
 
 sentry_sdk.init(
     dsn="https://d86e72394ab3886fccdeb4ec8631e6c7@o4511979386044416.ingest.us.sentry.io/4511979386306560",
-    traces_sample_rate=1.0,
+    traces_sample_rate=0.1,
 )
 
 import sys
